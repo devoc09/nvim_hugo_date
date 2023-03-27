020cafc102dee63ebcdfb3a3d75c8ee261074d19
devoc <50615605+devoc09@users.noreply.github.com>
2023-03-27 16:40:51 +0900

    Update hugo_date.lua

--- plugin/hugo_date.lua
+++ plugin/hugo_date.lua
@@ -4,4 +4,4 @@ end
 
 local map = vim.api.nvim_set_keymap
 
-map('n', '<S-d>', ":lua require('current_date').Insert()<cr>", {noremap=true, silent=true})
+map('n', '<C-d>', ":lua require('current_date').Insert()<cr>", {noremap=true, silent=true})
